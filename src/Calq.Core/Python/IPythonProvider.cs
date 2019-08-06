1d32945ec857149be5e6b602490ca3c4ffccae4a
Tim <tipfom@hotmail.de>
2019-08-06 12:31:30 +0200

    added sympy library

---
+++ src/Calq.Core/Python/IPythonProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calq.Core.Python
+{
+    interface IPythonProvider
+    {
+        string Evaluate(string expression);
+    }
+}
