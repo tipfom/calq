40cd0ed16479ba326ee2f6db6a6da9b1368c4693
Tim <tipfom@hotmail.de>
2019-08-08 11:57:56 +0200

    fixed MathView cutting off LaTeX

---
+++ src/Calq/LogMathView.cs
@@ -0,0 +1,21 @@
+﻿using CSharpMath.Forms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Calq
+{
+    class LogMathView : MathView
+    {
+        public LogMathView()
+        {
+        }
+
+        public override SizeRequest GetSizeRequest(double widthConstraint, double heightConstraint)
+        {
+            var rect = base.Measure;
+            return new SizeRequest(new Size(rect.Value.Width, rect.Value.Height+2));
+        }
+    }
+}
