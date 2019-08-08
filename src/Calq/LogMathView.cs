114b92ccdfab9b9d9afc9d4615033c8db0645291
Tim <tipfom@hotmail.de>
2019-08-08 15:30:54 +0200

    fixed LaTeX cutting when differetiating

--- src/Calq/LogMathView.cs
+++ src/Calq/LogMathView.cs
@@ -15,7 +15,8 @@ namespace Calq
         public override SizeRequest GetSizeRequest(double widthConstraint, double heightConstraint)
         {
             var rect = base.Measure;
-            return new SizeRequest(new Size(rect.Value.Width, rect.Value.Height+2));
+            if (rect == null) return base.GetSizeRequest(widthConstraint, heightConstraint);
+            return new SizeRequest(new Size(rect.Value.Width, rect.Value.Height+5));
         }
     }
 }
